2ddcd1459e150bc37a6c18be540777679efb3403
Robotizo <geniusrobotizo@gmail.com>
2023-02-24 09:58:47 -0800

    Posting Code

---
+++ README.md
@@ -0,0 +1,9 @@
+[Automated Number Plate Recognition]
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
+
+With this app you upload items to S3 from S3 a python script runs and views the images and reads the characters
---
+++ src/Components/items.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '../CSS/main.css'
+import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+
+var AWS = require("aws-sdk");
+AWS.config.update({
+    accessKeyId: 'AKIAIGEJPNVKRCGX3D6A', secretAccessKey: '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', region: 'ca-central-1'});
+var s3 = new AWS.S3();
+
+function refreshPage() {
+    window.location.reload(false);
+  }
+
+export default class GetImages extends React.Component{
+    state = {
+        loading: true, 
+        images: null
+    }
+
+
+    
+
+ 
+
+    async componentDidMount(){
+        const response = await s3.listObjectsV2({ Bucket: 'anprlinkbase' }).promise();
+        var contents = response.Contents;
+        console.log(contents[0]);
+        console.log(contents[0].LastModified );
+        this.setState({ images: contents, loading: false })
+    }
+
+
+    render(){
+        return(
+            <div>
+                {this.state.loading || !this.state.images ? (
+                    <div><CircularProgress /></div>
+                ):(   
+                    <div>
+                        <Grid container spacing={2}>
+                        {this.state.images.reverse().map(image =>
+                            
+                                <Grid item xs={3}>
+                                    <img src={ "https://anprlinkbase.s3.ca-central-1.amazonaws.com/" + image.Key } className="image" />
+                                    <div>{ image.LastModified.toString() }</div>
+                                </Grid>
+                            
+                    
+                        )}
+
+                        </Grid>
+                    </div>
+                )
+                }
+            </div>
+        )
+    }
+}
